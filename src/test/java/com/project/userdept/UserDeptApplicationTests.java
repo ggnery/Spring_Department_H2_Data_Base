e2bd401816ac21fc7888659abe40219676fa4194
xxGabrielNeryxx <gabrielgildino@gmail.com>
2024-01-24 08:51:05 -0300

    Initial commit

---
+++ src/test/java/com/project/userdept/UserDeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDeptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
